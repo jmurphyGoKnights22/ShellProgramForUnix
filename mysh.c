ff58d95ee1e884a50cea96860bbbb35b3a91e581
Dpk417182 <derrickkeough@yahoo.com>
2021-10-23 17:40:41 -0400

    simplified tokenizing; strtok_r() -> strtok()

--- mysh.c
+++ mysh.c
@@ -5,11 +5,12 @@ int main(void) {
   printf("# ");
   char str[120];
   fgets(str, 120, stdin); // store line from stdin into str
-  char* rest = str; // copy to be used by strtok_r() to maintain context between successive calls
-  char* token;
+  
+  char* token = strtok(str, " ");
 
-  while ((token = strtok_r(rest, " ", &rest))) {
+  while (token != NULL) {
     printf("%s\n", token);
+    token = strtok(NULL, " ");
   }
 
   return 0;
