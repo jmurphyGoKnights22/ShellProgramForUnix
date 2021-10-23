85dfd0ed1ec024598d0bf72b8d06ccf114611aa8
Dpk417182 <derrickkeough@yahoo.com>
2021-10-23 17:36:43 -0400

    simple tokenizing functionality

--- mysh.c
+++ mysh.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
   printf("# ");
+  char str[120];
+  fgets(str, 120, stdin); // store line from stdin into str
+  char* rest = str; // copy to be used by strtok_r() to maintain context between successive calls
+  char* token;
+
+  while ((token = strtok_r(rest, " ", &rest))) {
+    printf("%s\n", token);
+  }
+
   return 0;
 }
