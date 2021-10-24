01607a134456406c2676f6f975dde118140abbc7
James Murphy <jmurphyinbox@gmail.com>
2021-10-23 20:22:22 -0400

    Added switch for function directory

--- mysh.c
+++ mysh.c
@@ -16,9 +16,80 @@ int main(void) {
       while (token != NULL) {
         printf("%s ", token);
         token = strtok(NULL, " \n");
+        switch (token) {
+          case 'movetodir' :
+            movetodir();
+            break;
+          case 'whereami' :
+            whereami();
+            break;
+          case 'history' :
+            history();
+            break;
+          case 'byebye' :
+            byebye();
+            break;
+          case 'replay' :
+            replay();
+            break;
+          case 'start' :
+            start();
+            break;
+          case 'background' :
+            background();
+            break;
+          case 'dalek' :
+            dalek();
+            break;
+          case 'repeat' :
+            repeat();
+            break;
+          case 'dalekall' :
+            dalekall();
+            break;
+        }
       }
       printf("\n");
   }
-
+  
   return 0;
+}
+void movetodir() {
+
+}
+
+void whereami() {
+
+}
+
+void history() {
+
+}
+
+void byebye() {
+
+}
+
+void replay() {
+
+}
+
+void start() {
+
+}
+
+void background() {
+
+}
+
+void dalek() {
+
+}
+
+void repeat() {
+
+}
+
+void dalekall() {
+
 }
